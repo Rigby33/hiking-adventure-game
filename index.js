21b8eb23c536da21bcbe641e76c999971167cacc
Catherine Lynn <catherineannlynn@gmail.com>
2018-03-11 08:25:41 -0400

    working on rendering grid and path

--- index.js
+++ index.js
@@ -1,71 +1,37 @@
-
-
-
-// function createPath() {
-// 	let row = 0;
-// 	let randomColumn = Math.floor(Math.random()*6);
-// 	let lastRandomColumn = Math.floor(Math.random()*6);
-// 	let firstCell = document.getElementById(`0${randomColumn}`);
-// 	let lastCell = document.getElementById(`10${lastRandomColumn}`);
-// 	if(firstCell) {
-// 		firstCell.classList.add('partOfPath');
-// 	} if(lastCell) {
-// 		lastCell.classList.add('partOfPath');
-// 	}
-// 	do {
-// 		createNewPath(row, randomColumn);
-// 		row = Math.floor(Math.random()*10);
-// 	} while (row <= 5)
-// }
-
-// //choose random direction of next path item
-// function createPathItem(row, column) {
-
-// }
-
-// //return directions available from current path cell
-
-
-// function createNewPath(row, column) {
-// 	for (let i = 0; i <= 6; i++) {
-// 		for (let j = 0; j <= 11; j++) {
-// 			let newXCell = Math.floor(Math.random()*(j-row) + row);
-// 			let newYCell = Math.floor(Math.random()*(i-column)+column);
-// 			document.getElementById(`${newXCell}${newYCell}`).classList.add('partOfPath');
-// 		}
-// 	}
-// }
-
-
 function createGrid() {
 	let matrix = [];
-	for (let x = 0; x <= 9; x++) {
+	for (let x = 0; x <= 10; x++) {
 		let row = [];
-		for(let y = 0; y <= 4; y++) {
+		for(let y = 0; y <= 5; y++) {
 			row.push({x, y});
-			// let newDiv = document.createElement('div');
-			// newDiv.className = 'newDiv';
-			// let textNode = document.createTextNode(`[${i}][${j}]`);
-			// newDiv.appendChild(textNode);
-			// newDiv.dataset.x = i;
-			// newDiv.dataset.y = j;
-			// newDiv.setAttribute('id', `${i}${j}`);
-			// document.getElementById('grid').appendChild(newDiv);
+			let newDiv = document.createElement('div');
+			newDiv.className = 'newDiv';
+			let textNode = document.createTextNode(`[${x}][${y}]`);
+			newDiv.appendChild(textNode);
+			newDiv.dataset.x = x;
+			newDiv.dataset.y = y;
+			newDiv.setAttribute('id', `${x}${y}`);
+			document.getElementById('grid').appendChild(newDiv);
 		}
 		matrix.push(row);
 	}
 	return matrix;
 }
 
-function createPath(matrix, row=9, column=Math.floor(Math.random()*4)) {
+function createPath(matrix, row=10, column=Math.floor(Math.random()*5)) {
 	if(row === 0) {
 		return matrix;
 	}
 	const nextNode = chooseNode(matrix, row, column);
 	nextNode.node = true;
 	matrix[nextNode.x][nextNode.y] = nextNode;
-	console.log(matrix);
 	createPath(matrix, nextNode.x, nextNode.y);
+	console.log(`${nextNode.x}${nextNode.y}`);
+	for (let i = 0; i < matrix.length; i++) {
+		if(document.getElementById(`${nextNode.x}${nextNode.y}`) && nextNode.node === true) {
+			document.getElementById(`${nextNode.x}${nextNode.y}`).classList.add('partOfPath');
+		}
+	}
 }
 
 function chooseNode(matrix, row, column) {
@@ -76,7 +42,7 @@ function chooseNode(matrix, row, column) {
 
 function possibleNodes(matrix, row, column) {
 	let nodes = [];
-	if(row !== 9) {
+	if(row !== 10) {
 		if(!matrix[row+1].node) {
 			nodes.push({x: row+1, y: column})
 		}
@@ -91,7 +57,7 @@ function possibleNodes(matrix, row, column) {
 			nodes.push({x: row, y: column-1})
 		}
 	}
-	if(column !== 4) {
+	if(column !== 5) {
 		if(!matrix[column+1].node) {
 			nodes.push({x: row, y: column+1})
 		}
@@ -100,4 +66,4 @@ function possibleNodes(matrix, row, column) {
 }
 
 const matrix = createGrid();
-console.log(createPath(matrix));
+createPath(matrix);
