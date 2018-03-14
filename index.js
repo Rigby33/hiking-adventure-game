6e43a25ef1bb9c086954b92d7a2d44abe1c9d585
Catherine Lynn <catherineannlynn@gmail.com>
2018-03-13 22:24:56 -0400

    working more on path generation and cleaning up rendering function. whoops.

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
+function render(matrix) {
+	for (let x = 0; x < matrix.length; x++) {
+		for(let y = 0; y < matrix[x].length; y++) {
+			let node = matrix[x][y];
+			let newDiv = document.createElement('div');
+			newDiv.className = 'newDiv';
+			newDiv.setAttribute('id', `${node.x}${node.y}`);
+			console.log(node);
+			if(node.node) {
+				newDiv.classList.add('partOfPath');
+			}
+			document.getElementById('grid').appendChild(newDiv);
+		}
+	}
+}
+
 function createGrid() {
 	let matrix = [];
 	for (let x = 0; x <= 10; x++) {
 		let row = [];
 		for(let y = 0; y <= 25; y++) {
-			row.push({x, y});
-			let newDiv = document.createElement('div');
-			newDiv.className = 'newDiv';
-			newDiv.dataset.x = x;
-			newDiv.dataset.y = y;
-			newDiv.setAttribute('id', `${x}${y}`);
-			document.getElementById('grid').appendChild(newDiv);
+			row.push({x: x, y: y})
 		}
 		matrix.push(row);
 	}
 	return matrix;
 }
 
-function createPath(matrix, row=10, column=Math.floor(Math.random()*25)) {
+function createPath(matrix, row=10, column) {
 	if(row === 0) {
 		return matrix;
 	}
+	if(!column) {
+		column = Math.floor(Math.random()*25);
+		matrix[row][column] = {x: row, y: column, node: true};
+	}
+	console.log(matrix);
 	const nextNode = chooseNode(matrix, row, column);
 	nextNode.node = true;
 	matrix[nextNode.x][nextNode.y] = nextNode;
 	createPath(matrix, nextNode.x, nextNode.y);
-	for (let i = 0; i < matrix.length; i++) {
-		if(document.getElementById(`${nextNode.x}${nextNode.y}`) && nextNode.node === true) {
-			document.getElementById(`${nextNode.x}${nextNode.y}`).classList.add('partOfPath');
-		}
-	}
 }
 
 function chooseNode(matrix, row, column) {
@@ -37,66 +47,26 @@ function chooseNode(matrix, row, column) {
 	return nodes[randomNumber];
 }
 
-// function possibleNodes(matrix, row, column) {
-// 	let nodes = [];
-// 	console.log(matrix.length);
-// 	if(row - 1 >= 1) {
-// 		nodes.push({x: row - 1, y: column})
-// 	}
-// 	if(column + 1 < matrix[row].length) {
-// 		nodes.push({x: row, y: column+1})
-// 	}
-// 	if(row + 1 < matrix.length) {
-// 		nodes.push({x: row - 1, y: column})
-// 	}
-// 	if(column - 1 >= 1) {
-// 		nodes.push({x: row, y: column-1})
-// 	}
-// 	return nodes;
-// }
-
 function possibleNodes(matrix, row, column) {
 	let nodes = [];
-	// if(row !== 12) {
-	// 		nodes.push({x: row+1, y: column});
-	// }
 	if(row !== 0) {
+		if(!matrix[row-1][column].node) {
 			nodes.push({x: row-1, y: column})
+		}
 	}
 	if(column !== 0) {
+		if(!matrix[row][column-1].node) {
 			nodes.push({x: row, y: column-1})
+		}
 	}
 	if(column !== 25) {
+		if(!matrix[row][column+1].node) {
 			nodes.push({x: row, y: column+1})
+		}
 	}
-	console.log(nodes);
 	return nodes;
 }
 
-// function possibleNodes(matrix, row, column) {
-// 	let nodes = [];
-// 	if(row !== 10) {
-// 		if(!matrix[row+1].node) {
-// 			nodes.push({x: row+1, y: column})
-// 		}
-// 	}
-// 	if(row !== 0) {
-// 		if(!matrix[row-1].node) {
-// 			nodes.push({x: row-1, y: column})
-// 		}
-// 	}
-// 	if(column !== 0) {
-// 		if(!matrix[column-1].node) {
-// 			nodes.push({x: row, y: column-1})
-// 		}
-// 	}
-// 	if(column !== 5) {
-// 		if(!matrix[column+1].node) {
-// 			nodes.push({x: row, y: column+1})
-// 		}
-// 	}
-// 	return nodes;
-// }
-
 const matrix = createGrid();
 createPath(matrix);
+render(matrix);
