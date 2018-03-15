eb31a57cc4dd2c8f695087a444b68e79ba6b04ab
Catherine Lynn <catherineannlynn@gmail.com>
2018-03-14 22:05:35 -0400

    deleting some things

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Grid from './components/grid';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Grid />, document.getElementById('root'));
-registerServiceWorker();
