ebf3c0dc5624d5bc5861c03a32d87b8dee2771d6
Catherine Lynn <catherineannlynn@gmail.com>
2018-04-26 15:44:58 -0400

    fixed test

--- src/actions/users.test.js
+++ src/actions/users.test.js
@@ -3,25 +3,26 @@ import {API_BASE_URL} from '../config';
 
 describe('registerUser', () => {
     it('Should call post on /user', () => {
-    	const user = {username: 'newuser', password: 'password'};
+    	const username = 'newuse';
+    	const password = 'newpassword';
         const highscore = 100;
         global.fetch = jest.fn().mockImplementation(() =>
             Promise.resolve({
                 ok: true,
                 json() {
-                    return JSON.stringify({user, highscore});
+                    return {username, password, highscore};
                 }
             })
         );
 
         const dispatch = jest.fn();
-        return registerUser()(dispatch).then(() => {
+        return registerUser({username, password, highscore})(dispatch).then(() => {
             expect(fetch).toHaveBeenCalledWith(`${API_BASE_URL}/user`, {
             	method: 'POST',
             	headers: {
             		'content-type': 'application/json'
             	},
-            	body: {user, highscore}
+            	body: JSON.stringify({user: {username, password, highscore}})
             });
         });
     });
