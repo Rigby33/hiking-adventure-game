069857b77e19ef085589edfaddff81b31daa36d0
Catherine Lynn <catherineannlynn@gmail.com>
2018-04-26 15:06:39 -0400

    fixed a test

--- src/actions/savescore.test.js
+++ src/actions/savescore.test.js
@@ -2,9 +2,7 @@ import {SAVE_SCORE, saveScore} from './savescore';
 
 describe('saveScore', () => {
 	it('Should return action', () => {
-		const score = 100;
-		const action = saveScore(score);
+		const action = saveScore();
 		expect(action.type).toEqual(SAVE_SCORE);
-		expect(action.score).toEqual(score);
 	})
 })
